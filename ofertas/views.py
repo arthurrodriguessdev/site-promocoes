aff7876b47e43b5d768d0c840d9422acfdfec475
Arthur <arthurdev.contatoo@gmail.com>
2025-07-26 11:02:59 -0300

    automatizando criacao de cards com dicionario

--- ofertas/views.py
+++ ofertas/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'ofertas/index.html')
+    ofertas = {
+        'ofertas': [
+            {'supermercado': 'Barbosa', 'descricao': 'arroz, feijao, macarrao'},
+            {'supermercado': 'Esperança', 'descricao': 'carne, travesseiro, chcocolate'},
+            {'supermercado': 'Brasil', 'descricao': 'sorvete, açaí, banana'},
+            {'supermercado': 'Central', 'descricao': 'Arroz, Feijão, Açúcar'},
+            {'supermercado': 'Nova Vida', 'descricao': 'Leite, Pão, Café'},
+            {'supermercado': 'Bom Preço', 'descricao': 'Sabão em pó, Detergente, Amaciante'},
+        ]
+    }
+
+    return render(request, 'ofertas/index.html', ofertas)
