895d0efb2c3b0b8b959de029904b20a163a249fe
Arthur <arthurdev.contatoo@gmail.com>
2025-07-29 18:30:36 -0300

    relacao one to many de usuarios, listas, itens em models

--- listas/models.py
+++ listas/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+#Relação One-to-many. UM usuário pode ter VÁRIAS listas de compras. UMA lista pode ter VÁRIOS produtos
+class ListaCompras(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listas') 
+
+    nome = models.CharField(max_length=100)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+class Item(models.Model):
+    lista = models.ForeignKey(ListaCompras, on_delete=models.CASCADE, related_name='itens')
+
+    nome = models.CharField(max_length=100)
+    quantidade = models.PositiveIntegerField(default=1)
 
