b6ef738cace17fbc8b36492b492cc9b1f6192962
Arthur <arthurdev.contatoo@gmail.com>
2025-08-07 16:15:15 -0300

    apagando uma view desnecessaria

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from ofertas.views import index, detalhamento_oferta, sobre_software, listar_ofertas
+from ofertas.views import detalhamento_oferta, sobre_software, listar_ofertas
 
 urlpatterns = [
-    path('index/', index, name='index'),
     path('detalhamento/<int:id>', detalhamento_oferta, name='detalhamento'),
     path('index/listar', listar_ofertas, name='listar_ofertas'),
     path('index/sobre', sobre_software, name='sobre_software'),
--- ofertas/views.py
+++ ofertas/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from ofertas.models import Oferta
 from django.contrib.auth.decorators import login_required
 
-def index(request):
-    oferta = Oferta.objects.all().order_by('supermercado__nome_supermercado') #Pega todos os objetos do banco de dados e salva na variável
-    ofertas = {
-        'ofertas': oferta
-    }
-
-    return render(request, 'ofertas/index.html', ofertas)
-
 @login_required(login_url='login')
 def detalhamento_oferta(request, id):
     oferta = get_object_or_404(Oferta, id=id) #Pega o objeto que tem o ID específico ou erro 404 
@@ -22,12 +14,11 @@ def detalhamento_oferta(request, id):
 
 
 def listar_ofertas(request):
+    oferta = Oferta.objects.all().order_by('supermercado__nome_supermercado')
     search = request.GET.get('search')
 
     if search:
         oferta = Oferta.objects.filter(supermercado__nome_supermercado__icontains=search).order_by('supermercado__nome_supermercado')
-    else:
-        oferta = Oferta.objects.all().order_by('nome_supermercado')
 
     ofertas = {
         'ofertas': oferta
